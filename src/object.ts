bec899f5067154b45892b900b6da1970d3486e64
Leon <tahjib.leon399@gmail.com>
2023-05-05 03:50:34 +0600

    object

---
+++ src/object.ts
@@ -0,0 +1,14 @@
+const users : {
+    readonly company: string,
+    name:string,
+    isMarried:boolean,
+    age:number,
+    wife?:string
+}  = {
+    company: "PH",
+    name : "Babul mia",
+    age : 32,
+    isMarried : true,
+   
+}
+
