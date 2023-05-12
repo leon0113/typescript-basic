8f378a17880ab63173423e9b7f523eacdab467fc
Leon <tahjib.leon399@gmail.com>
2023-05-12 21:55:48 +0600

    interface in object

--- src/Module-3/interface.ts
+++ src/Module-3/interface.ts
@@ -7,7 +7,8 @@ type extendedUser = User & {
     role : string;
 }
 
-//! interface
+
+//! interface / can be used only in objects / fuunctions/ array
 interface IUser{
     name : string,
     age : number
@@ -23,6 +24,16 @@ const user1 : IExtendedUser = {
     role : 'web developer'
 }
 
+// console.log(user1);
+
+//? for functions  (type preffered)
+
+interface IAddNumbers {
+    (num1 : number, num2 : number) : number;
+}
+
+const addNumbers : IAddNumbers = (num1,num2) => num1 + num2;
+
 // const userWithTypeAlias : User = {
 //  name : 'leon',
 //  age : 34
@@ -32,3 +43,19 @@ const user1 : IExtendedUser = {
 //     name : 'leon I',
 //     age : 43
 // }
+
+
+//? for array (preffered type)
+
+// type 
+ 
+type roll1Type = number[];
+  
+const roll1 : roll1Type = [23,43,12,4,5];
+
+// interface 
+interface IRoll2{
+    [index : number] : number;
+}
+
+const roll2 : IRoll2 = [2,4,5,6,6,7];
